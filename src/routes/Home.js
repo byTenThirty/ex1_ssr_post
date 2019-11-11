733e302abee20090a354cf078c9b6dfdb78c14a2
frontdigger <frontdigger@gmail.com>
2019-11-11 23:23:00 +0900

    테이블추가

--- src/routes/Home.js
+++ src/routes/Home.js
@@ -2,9 +2,17 @@ import React, { useEffect, useState } from 'react';
 import { Link } from 'react-router-dom';
 import axios from 'axios';
 import '../App.css';
+import Paper from '@material-ui/core/Paper';
+import Table from '@material-ui/core/Table';
+import TableBody from '@material-ui/core/TableBody';
+import TableCell from '@material-ui/core/TableCell';
+import TableHead from '@material-ui/core/TableHead';
+import TableRow from '@material-ui/core/TableRow';
+import TablePagination from '@material-ui/core/TablePagination';
 
 const Home = () => {
 	const [posts, setPosts] = useState([]);
+	const [page, setPage] = React.useState(0);
 
 	useEffect(() => {
 		axios.get('https://jsonplaceholder.typicode.com/posts')
@@ -13,23 +21,68 @@ const Home = () => {
 			});
 	}, []);
 
+	const handleChangePage = (event, newPage) => {
+		setPage(newPage);
+	};
+	
+
 	return (
 		<div className="App">
 			<div className="list-title">
-				<h3>클라이언트사이드 리스트</h3>
+				<h3>서버사이드 리스트</h3>
 			</div>
-			<div className="list-content">
-				<ul>
-					{posts.map(post =>
-						<Link key={post.id} to={`/post/${post.id}`}>
-							<li key={post.id} className="list-li">
-								{post.title}
-								<span className="li-span">></span>
-							</li>
-						</Link>
-					)}
-				</ul>
-			</div>
+			<Paper >
+					<div >
+						<Table stickyHeader aria-label="sticky table">
+						<TableHead>
+							<TableRow>
+							
+								<TableCell align="center">
+									ID
+								</TableCell>
+								<TableCell align="center">
+									제목
+								</TableCell>
+								<TableCell align="center">
+									본문
+								</TableCell>
+							
+							</TableRow>
+						</TableHead>
+						<TableBody>
+							{posts.slice(page * 5, page * 5 + 5).map((row, index) => {
+								return (
+									<TableRow hover role="checkbox" tabIndex={-1} key={row.id}>
+										<TableCell key={`${row.id}`} align="center">
+											{row.id}
+										</TableCell>
+										<TableCell key={`${row.id}_${index}`} align="center">
+											{row.title}
+										</TableCell>
+										<TableCell key={index} align="center">
+											{row.body}
+										</TableCell>
+									</TableRow>
+								);
+							})}	
+						</TableBody>
+					</Table>
+				</div>
+				<TablePagination
+					rowsPerPageOptions={[]}
+					component="div"
+					count={posts.length}
+					rowsPerPage={5}
+					page={page}
+					backIconButtonProps={{
+						'aria-label': 'previous page',
+					}}
+					nextIconButtonProps={{
+						'aria-label': 'next page',
+					}}
+					onChangePage={handleChangePage}
+				/>
+			</Paper>
 		</div>
 	);
 };
